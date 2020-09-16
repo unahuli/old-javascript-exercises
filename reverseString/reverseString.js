78ea31cbf8b8e0da6b9dfbc510cb381d726b945a
jian <gray.ratsky@gmail.com>
2020-09-16 23:48:59 +0800

    Finished reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversedWord = '';
+    let length = word.length - 1;
+    for (let i = length; i >= 0; i--) {
+        reversedWord += word[i];
+    }
+    return reversedWord;
 }
 
 module.exports = reverseString
