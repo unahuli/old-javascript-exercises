fbd257e4f1966ab3f2702d3662d50d1bb5ebaaf8
jian <gray.ratsky@gmail.com>
2020-09-19 00:45:35 +0800

    Completed removeFromArray and sumAll exercises.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
 const removeFromArray = function() {
+    let index;
+    let array = arguments[0];
+    let element;
+    // if (arguments.length < 3) {
+    //     element = arguments[1]; 
+    //     index = array.indexOf(element);
+    //         if (index != -1) {
+    //             array.splice(index, 1);
+    //         }
+    //     return array;
+    // } else {
+        for (let i = 1; i < arguments.length; i++) {
+            element = arguments[i]; 
+            index = array.indexOf(element);
+            if (index != -1) {
+                array.splice(index, 1);
+            }
+        }
+        return array;           
+    //}
 
 }
 
--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let total = 0;
+    if (firstNum < 0 || secondNum < 0 || typeof(firstNum) != 'number' || typeof(secondNum) != 'number') {
+        return 'ERROR';
+    } else if (secondNum > firstNum) {
+        for(let i = firstNum; i <= secondNum; i++) {
+            total += i;
+        }
+    } else {
+        for(let i = secondNum; i <= firstNum; i++) {
+            total += i;
+        }
+    }
+    return total;
 }
 
 module.exports = sumAll
