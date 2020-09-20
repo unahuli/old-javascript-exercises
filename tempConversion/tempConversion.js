dd84a1c9b1d70fa9a4a3ce09e09268a06e3294d7
jian <gray.ratsky@gmail.com>
2020-09-20 23:40:31 +0800

    Finished leapYears and tempConversion exercises.

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahTemp) {
+  return roundToOne(((fahTemp - 32) * 5)/9);
 }
 
-const ctof = function() {
+const ctof = function(celTemp) {
+  return roundToOne((celTemp * 9) / 5 + 32);
+}
 
+const roundToOne = function(number) {
+  return Math.round(number * 10) / 10;
 }
 
 module.exports = {
